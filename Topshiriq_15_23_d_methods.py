0820dd8a6a5c57ab4e48a0219065461b73e4963a
farxodelamasov <farxodflbbank@gmail.com>
2024-12-02 23:52:18 +0500

    Three Methods Tasks done

--- Topshiriq_15_23_d_methods.py
+++ Topshiriq_15_23_d_methods.py
@@ -152,10 +152,188 @@ class Book:
         self.page = self.first_page
 
 
-a_book = Book("Kichkina shahzoda", "Antuan de Sent-Ekzyuperi", 88)
-a_book.open(33)
-a_book.turn_page()
-a_book.turn_page()
-a_book.restart()
-a_book.turn_page()
-a_book.turn_page()
+# a_book = Book("Kichkina shahzoda", "Antuan de Sent-Ekzyuperi", 88)
+# a_book.open(33)
+# a_book.turn_page()
+# a_book.turn_page()
+# a_book.restart()
+# a_book.turn_page()
+# a_book.turn_page()
+
+
+## 6
+class Dog:
+    total_dogs = 0
+
+    @classmethod
+    def __init__(cls):
+        Dog.total_dogs += 1
+
+    @classmethod
+    def get_total_dogs(cls):
+        print(cls.total_dogs)
+
+
+# dog_1 = Dog()
+# dog_2 = Dog()
+# dog_3 = Dog()
+#
+# Dog.get_total_dogs()
+
+
+## 7
+class Computer:
+    total_computers = 0
+    computers_list = []
+
+    @classmethod
+    def add_computer(cls, name_obj):
+        cls.total_computers += 1
+        cls.computers_list.append(name_obj)
+
+
+# com = Computer()
+# com.add_computer("com_1")
+# com.add_computer("com_2")
+# com.add_computer("com_3")
+#
+# print(Computer.total_computers, Computer.computers_list)
+
+
+## 8
+class Employee:
+    total_employees = 0
+    employees_list = []
+
+    @classmethod
+    def hire_employee(cls, emp_name):
+        cls.total_employees += 1
+        cls.employees_list.append(emp_name)
+
+
+# emp = Employee()
+# emp.hire_employee("John")
+# emp.hire_employee("Doe")
+# emp.hire_employee("Ali")
+#
+# print(Employee.total_employees, Employee.employees_list)
+
+
+## 9
+class Television:
+    total_televisions = 0
+    total_screen_size = 0
+    average_screen_size = 0
+
+    @classmethod
+    def __init__(cls):
+        cls.total_televisions += 1
+
+    @classmethod
+    def update_average_screen_size(cls, screen_size):
+        cls.total_screen_size += screen_size
+        cls.average_screen_size = cls.total_screen_size / cls.total_televisions
+
+
+# tv_1 = Television()
+# tv_1.update_average_screen_size(43)
+# tv_2 = Television()
+# tv_2.update_average_screen_size(50)
+# tv_3 = Television()
+# tv_3.update_average_screen_size(60)
+#
+# print(f"Televizorlar soni: {Television.total_televisions}")
+# print(f"O'rtacha ekran: {Television.average_screen_size}")
+
+## 10
+class Course:
+    total_courses = 0
+    courses_list = []
+
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+
+    @classmethod
+    def add_course(cls, kurs):
+        cls.courses_list.append(kurs)
+        cls.total_courses += 1
+
+
+course_1 = Course(name="Matematika", desc="Uy ishi: 3-mavzu misollari")
+course_2 = Course(name="Tarix", desc="Uy ishi: 12-mavzu takrorlash")
+
+Course.add_course(course_1)
+Course.add_course(course_2)
+
+
+# print("Kurslar soni:", Course.total_courses)
+# for course in Course.courses_list:
+#     print(f"Kurs nomi: {course.name}, Description: {course.desc}")
+
+
+## 11
+class Math:
+
+    @staticmethod
+    def multiply(a, b):
+        c = a + b
+        return c
+
+
+# print(Math.multiply(3, 5))
+
+
+## 12
+class Temperature:
+
+    @staticmethod
+    def celsius_to_fahrenheit(celsius):
+        fahrenheit = (celsius * 9) / 5 + 32
+        return celsius, fahrenheit
+
+
+celsius, fahrenheit = Temperature.celsius_to_fahrenheit(30)
+
+# print(f"Celsius: {celsius} \nFahrenheit: {fahrenheit}")
+
+## 13
+class Distance:
+
+    @staticmethod
+    def miles_to_kilometers(miles_1):
+        km_1 = miles_1 * 1.609344
+        km_1 = round(km_1, 2)
+
+        return miles_1, km_1
+
+miles, km = Distance.miles_to_kilometers(20)
+
+# print(f"Miles: {miles} \nKilometers: {km}")
+
+
+## 14
+class Utility:
+
+    @staticmethod
+    def is_even(num):
+        if num % 2 == 0:
+            return True
+        else:
+            return False
+
+# print(Utility.is_even(5))
+
+
+## 15
+class Time:
+
+    @staticmethod
+    def seconds_to_minutes(seconds):
+        minutes = seconds // 60
+        second = seconds - minutes * 60
+        return seconds, minutes, second
+
+seconds_1, minute, sec = Time.seconds_to_minutes(100)
+
+# print(f"Seconds: {seconds_1} is >>> Minutes: {minute} and seconds: {sec}")
--- bilim_1.py
+++ bilim_1.py
@@ -81,13 +81,67 @@
 # Find() bilan Index() string metodlari orasidagi farq shuki Agar find()da yo'q narsani indeksi so'ralsa -1 qaytaradi index()dan so'ralsa xatolik qaytaradi.
 
 
-class Solution(object):
-    def equalFrequency(self, word):
-        print(word)
+# class Solution(object):
+#     def equalFrequency(self, word):
+#         print(word)
+#
+#
+# Solution().equalFrequency("abcc")
 
 
-Solution().equalFrequency("abcc")
+# class Course:
+#     # Class-level attributes
+#     total_courses = 0
+#     courses_list = []
+#
+#     def __init__(self, name, description):
+#         # Instance-level attributes
+#         self.name = name
+#         self.description = description
+#
+#     @classmethod
+#     def add_course(cls, course):
+#         # Adding course instance to courses_list and updating total_courses
+#         cls.courses_list.append(course)
+#         cls.total_courses += 1
+#
+#
+# # Example usage:
+# course1 = Course("Mathematics", "Learn algebra, geometry, and calculus.")
+# course2 = Course("Physics", "Understand the fundamentals of motion and energy.")
+#
+# Course.add_course(course1)
+# Course.add_course(course2)
+#
+# print("Total courses:", Course.total_courses)
+# for course in Course.courses_list:
+#     print(f"Course Name: {course.name}, Description: {course.description}")
 
+#
+# class Television:
+#     # Class-level attribute
+#     average_screen_size = 0
+#     _total_screen_size = 0
+#     _total_televisions = 0
+#
+#     def __init__(self, screen_size):
+#         self.screen_size = screen_size
+#         Television._total_screen_size += screen_size
+#         Television._total_televisions += 1
+#         self.update_average_screen_size()
+#
+#     @classmethod
+#     def update_average_screen_size(cls):
+#         if cls._total_televisions > 0:
+#             cls.average_screen_size = cls._total_screen_size / cls._total_televisions
 
-
+# Example usage
+# tv1 = Television(42)
+# print(f"Average Screen Size after tv1: {Television.average_screen_size}")
+#
+# tv2 = Television(55)
+# print(f"Average Screen Size after tv2: {Television.average_screen_size}")
+#
+# tv3 = Television(65)
+# print(f"Average Screen Size after tv3: {Television.average_screen_size}")
 
